bb6e921f68022625831e563559e8d084d2659a6a
veronicaosoriodurango <veroosoriood@gmail.com>
2025-04-21 18:48:31 -0500

    Add files via upload

---
+++ ejercicio1.py
@@ -0,0 +1,13 @@
+
+
+edad = int(input("ingrese la edad: "))
+if edad >= 18 and edad<=100:
+    print("es mayor de edad")
+
+if edad >= 0:
+    print("edad invalida")
+elif edad>=1 and edad<18:
+    print("es menor de edad")
+else:
+    print("es un bebe")
+
---
+++ ejercio2.py
@@ -0,0 +1,8 @@
+
+
+numero = int(input("ingrese numero: "))
+if numero >= 0:
+    print("es positivo")
+
+else:
+    print("es negativo")
